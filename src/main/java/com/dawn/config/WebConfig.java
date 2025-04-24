44868044cff09b2fafc0ac1580f63e90fb8537b5
Jet <dev@com>
2025-04-24 21:39:42 +0800

    fix: 优化CORS配置

--- src/main/java/com/dawn/config/WebConfig.java
+++ src/main/java/com/dawn/config/WebConfig.java
@@ -7,11 +7,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    // 在开发时，通常 Vue 项目运行在本地（如 http://localhost:8080），
-    // 而 Spring Boot 后端运行在另一个端口（如 http://localhost:8081）。
-    // 由于端口不同，浏览器会认为这是跨源请求，你需要在 Spring Boot 中配置 CORS，允许前端访问后端 API。
+    //  前端项目与后端项目，由于端口不同，跨源请求。
     @Override
-    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Allow CORS for all API endpoints under /api
                 .allowedOrigins("http://localhost:5173") // Allow requests from the default Vite dev server origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods

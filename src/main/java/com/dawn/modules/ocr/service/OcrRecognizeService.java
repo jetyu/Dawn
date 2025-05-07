9c2dfbc47d0feb6ad74e7bad9ab05fb9c1058495
Jet <Jet Studio>
2025-05-07 20:50:41 +0800

    feat:删除百度OCR和相关接口

--- src/main/java/com/dawn/modules/ocr/service/OcrRecognizeService.java
+++
@@ -1,8 +0,0 @@
-package com.dawn.modules.ocr.service;
-
-public interface OcrRecognizeService {
-    /**
-     * 对文件进行OCR识别，返回识别文本。
-     */
-    String recognize(java.io.File file);
-}
--- src/main/java/com/dawn/modules/ocr/service/OcrResultService.java
+++
@@ -1,7 +0,0 @@
-package com.dawn.modules.ocr.service;
-
-import com.dawn.modules.ocr.dto.OcrResultDto;
-
-public interface OcrResultService {
-    OcrResultDto saveOcrResult(OcrResultDto dto);
-}
